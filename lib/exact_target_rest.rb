249d49f44d93ad1b993094251e9c5e1e6ee07d97
Andrea Di Persio <andrea.dipersio@shopify.com>
2020-11-27 11:14:49 +0100

    Move to v2 authentication
    Since 2019 is not possible anymore to create new connected apps
    using the legacy api (v1).

--- lib/exact_target_rest.rb
+++ lib/exact_target_rest.rb
@@ -3,10 +3,8 @@ require 'faraday_middleware'
 
 module ExactTargetRest
   FARADAY_ADAPTER = :net_http
-  AUTH_URL = 'https://auth.exacttargetapis.com/v1/requestToken'
-  TRIGGERED_SEND_URL = 'https://www.exacttargetapis.com'
+  AUTH_PATH = '/v2/token'
   TRIGGERED_SEND_PATH = '/messaging/v1/messageDefinitionSends/key:%s/send'
-  DATA_EXTENSION_URL = 'https://www.exacttargetapis.com'
   DATA_EXTENSION_PATH = '/hub/v1/dataevents/key:%s/rowset'
 end
 
